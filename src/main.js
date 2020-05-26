16a2d873a10a0c38586168eae8d688900f217464
苏蓉彬 <746996050@qq.com>
2020-05-27 00:27:40 +0800

    feature: 定义方法

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@
  * @Author: cooky
  * @Date: 2020-05-14 17:30:02
  * @LastEditors: cooky
- * @LastEditTime: 2020-05-14 17:32:45
+ * @LastEditTime: 2020-05-26 14:34:55
  */ 
 import Vue from 'vue'
-// import App from './App.vue'
-import GlobalError from '@/components/ErrorLog'
+import App from './App.vue'
+import GlobalError from '@/packages'
 
-Vue.use(GlobalError)
+Vue.use(GlobalError, {
+  url: 'http://10.46.199.205:8895/distribute',
+  logType: 'dali'
+})
 Vue.config.productionTip = false
 
 new Vue({
--- src/packages/index.js
+++ src/packages/index.js
@@ -3,19 +3,19 @@
  * @Author: cooky
  * @Date: 2020-05-14 17:31:55
  * @LastEditors: cooky
- * @LastEditTime: 2020-05-15 11:01:35
+ * @LastEditTime: 2020-05-26 14:36:32
  */ 
 import { uploadLog } from '@/api/log'
-// const errorHandler = function (err, vm, info) {
-//   const errorParams = {
-//     type: 'code',
-//     errorMsg: err.message,
-//     errorCode: 203001,
-//     stack: err.stack
-//   }
-//   console.log(err.message, errorParams)
-//   uploadLog(errorParams)
-// }
+const errorHandler = function (err) {
+  const errorParams = {
+    type: 'code',
+    errorMsg: err.message,
+    errorCode: 203001,
+    stack: err.stack
+  }
+  console.log(err.message, errorParams)
+  uploadLog(errorParams)
+}
 // 当发⽣生 JavaScript 运⾏行行时错误（包括处理理程序中引发的语法错误和异常）时，使⽤用接⼝口 ErrorEvent 的 error 事件将在 window 被触发，并被 window.onerror() 调⽤用
 // window.onerror =  (msg, url, lineNo, columnNo, e) => {
 //     console.log(msg, url, lineNo, columnNo, e)
@@ -34,23 +34,18 @@ import { uploadLog } from '@/api/log'
 //   }
 // })
 
-class GlobalError {
-  static instance;
-  static getInstance () {
-    if(!this.instance) {
-        this.instance = new GlobalError(name);
-    }
-    return this.instance;
-  }
-  constructor() {
-    this.instance = null
-  }
+const GlobalError = {
+  install (Vue, {url, logType}) {
+    this.apiUrl = url
+    this.logType = logType
+    this.initEvent()
+    this.initVueEvent(Vue)
+  },
   init ({url, logType}) {
     this.apiUrl = url
     this.logType = logType
     this.initEvent()
-    this.initVueEvent()
-  }
+  },
   initGlobalEvent () {
     window.onerror =  (msg, url, lineNo, columnNo, e) => {
         console.log(msg, url, lineNo, columnNo, e)
@@ -66,8 +61,8 @@ class GlobalError {
           errorHandler(e.reason)
       }
     })
-  }
-  initVueEvent () {
+  },
+  initVueEvent (Vue) {
     Vue.config.errorHandler = errorHandler
     Vue.mixin({
       beforeCreate () {
@@ -93,7 +88,7 @@ class GlobalError {
       }
     })
     Vue.prototype.$throw = errorHandler
-  }
+  },
   error (err, { type = 'code', errorCode = 203001 }) {
     const errorParams = {
       type,
@@ -105,16 +100,16 @@ class GlobalError {
     uploadLog(errorParams, this.apiUrl)
   }
 }
-function install ({url, logType}) {
-  if (install.installed) {
-    return false;
-  }
-
-  install.installed = true;
-}
-GlobalError.install = install
-
 export default GlobalError
+
+  // function install ({url, logType}) {
+  //   if (install.installed) {
+  //     return false
+  //   }
+  //   install.installed = true
+  //   console.log('install: ', url, logType)
+  // }
+  // GlobalError.install = install
 // const GlobalError = {
 //   install (Vue) {
 //     Vue.config.errorHandler = errorHandler
